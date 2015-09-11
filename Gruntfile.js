7466903c9243dd49a606696674215c877660461e
Michael Gray <michael.jgray01@gmail.com>
2015-09-11 18:30:46 -0500

    updated Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -404,7 +404,7 @@ module.exports = function (grunt) {
     },
     heroku: {
       options: {
-        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        remote: 'git@heroku.com:fathomless-escarpment-5725.git',
         branch: 'master',
         tag: pkg.version
       }
